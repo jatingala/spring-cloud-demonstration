08ecfd455060831bb0cae6b0916e88a5c6487467
Jatin Gala <jatin.gala@outlook.com>
2022-05-22 10:22:09 +0530

    add code for config server, eureka server, gateway server

---
+++ gateway-server/src/main/java/com/jatin/springcloud/demo/gatewayserver/GatewayServerApplication.java
@@ -0,0 +1,17 @@
+package com.jatin.springcloud.demo.gatewayserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@RefreshScope
+@EnableEurekaClient
+@SpringBootApplication
+public class GatewayServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatewayServerApplication.class, args);
+	}
+
+}
